04d82bae722106bcf842174a82f9a76c58858941
PanelSDNpreset <barakarahadiannezar@gmail.com>
2024-05-29 12:55:51 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6285784424998','6285784424998','6285784424998'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6285784424998'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6285784424998' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'SDN_Preset' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'SDN-PRESET' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'SDN_Preset' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -41,8 +41,8 @@ global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© SDN-PRESET' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'SDN_Preset' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -52,7 +52,7 @@ global.sessionName = 'session'
 //terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
-free: 15,
+free: 5,
 }
 
 //—————「 Set Image 」—————//
@@ -91,7 +91,7 @@ private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
 bot: '_Fitur Khusus Pengguna Nomor Bot_',
 wait: '_Sedang Di Proses..._',
 error: '_Fitur Sedang Error!_',
-endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+endLimit: '_Limit Harian Anda Telah Habis , Silakan Beli Premium Untuk Mendapatkan Limit Unlimited _',
 }
 
 //—————「 Batas Akhir 」—————//
